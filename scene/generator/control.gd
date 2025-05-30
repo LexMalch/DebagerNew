89761fe2b664f71393986924ebcbc9ca341ca32a
Мальчуков Алексей <106741316+LexMalch@users.noreply.github.com>
2025-05-30 20:18:50 +0700

    asdfasdf

--- scene/generator/control.gd
+++ scene/generator/control.gd
@@ -2,4 +2,18 @@ extends Control
 
 
 func _on_button_pressed() -> void:
-	print("ad[p;kasf]")
+	$HBoxContainer/LaserDamage.hide()
+	await get_tree().create_timer(5).timeout
+	$HBoxContainer/LaserDamage.show()
+
+
+func _on_button_2_pressed() -> void:
+	$HBoxContainer/TurretDamage.hide()
+	await get_tree().create_timer(5).timeout
+	$HBoxContainer/TurretDamage.show()
+
+
+func _on_button_3_pressed() -> void:
+	$HBoxContainer/OreAmount.hide()
+	await get_tree().create_timer(5).timeout
+	$HBoxContainer/OreAmount.show()
